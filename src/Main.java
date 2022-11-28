6e947dd077c53b8805ebb5cd99fb8ebfc2bedb95
Alain Jordan <ajmartinezq@minsait.com>
2022-11-28 13:08:47 -0600

    Creacion del proyecto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,5,7,2,4,2,5,6,82,1,2);
+        
+    }
+}
